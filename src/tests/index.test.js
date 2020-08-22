c9d6198ab241a29978157e7e3d288f32a66800dd
Frazer Bayley <14934024+FrazerBayley@users.noreply.github.com>
2020-08-21 17:26:20 -0700

    fix: multi contaminations (#51)
    * fix: multi contaminations
    
    * fix: only contaminate active exps

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -731,4 +731,23 @@ describe('Evolv client unit tests', () => {
       emit(context, EFFECTIVE_GENOME_UPDATED, {});
     });
   })
+
+  describe('contaminate', () => {
+    it('should contaminate once', () => {
+      store.activeEntryPoints = () => new Promise((resolve, reject) => { resolve(['1234']) });
+      Object.defineProperty(store, 'configuration', { get: function() { return { foo: 'bar' }; }, });
+      Object.defineProperty(store, 'activeEids', { get: function() { return new Set(['1234']); } });
+      options.store = store;
+
+      context.initialize()
+      context.set("experiments.allocations", [{eid: '1234', cid: '5678'}])
+      options.context = context;
+
+      const client = new Evolv(options);
+      client.contaminate();
+
+      expect(context.remoteContext.contaminations).to.be.lengthOf(1);
+      expect(context.remoteContext.contaminations[0].cid).to.be.equal('5678');
+    });
+  })
 });
