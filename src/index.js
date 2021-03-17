bd2eb9ebd27627a627437b7bede3adfe418c8f22
robertsevernsentient <robert.severn@evolv.ai>
2021-03-16 20:29:14 -0700

    feat: block beacon (#78)
    * feat: block beacon
    
    config to block the beacon and method call to unblock beacon call
    * feat: rename option
    * feat: rename method
    
    RKT-8822

--- src/index.js
+++ src/index.js
@@ -43,8 +43,8 @@ function EvolvClient(options) {
 
   const store = options.store || new Store(options);
   const context = options.context || new Context(store);
-  const contextBeacon = options.analytics ? new Beacon(options.endpoint + '/' + options.environment + '/data', context) : null;
-  const eventBeacon = options.beacon || new Beacon(options.endpoint + '/' + options.environment + '/events', context);
+  const contextBeacon = options.analytics ? new Beacon(options.endpoint + '/' + options.environment + '/data', context, options.bufferEvents) : null;
+  const eventBeacon = options.beacon || new Beacon(options.endpoint + '/' + options.environment + '/events', context, options.bufferEvents);
 
   /**
    * The context against which the key predicates will be evaluated.
@@ -403,6 +403,18 @@ function EvolvClient(options) {
     eventBeacon.flush();
     if (options.analytics) {
       contextBeacon.flush();
+    }
+  };
+
+  /**
+   * If the client was configured with
+   * bufferEvents: true
+   * then calling this will allow data to be sent back to Evolv
+   */
+  this.allowEvents = function() {
+    eventBeacon.unblockAndFlush();
+    if (options.analytics) {
+      contextBeacon.unblockAndFlush();
     }
   };
 
