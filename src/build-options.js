87a6861d8dd943a86f8cd941ce4470a6170f1f38
Matt Strom <matt.strom@evolv.ai>
2021-12-20 11:14:05 -0800

    feat(AP-773): support for request hooks
    [AP-773]

--- src/build-options.js
+++ src/build-options.js
@@ -1,11 +1,17 @@
 import { assign } from './ponyfills/objects.js';
 
 /**
+ * @typedef RequestHooks
+ * @property {(options: RetrieveOptions) => RetrieveOptions} [beforeOptions]
+ */
+
+/**
  * @typedef EvolvClientOptions
  * @property {string} environment
  * @property {number} version
  * @property {string} endpoint
  * @property {boolean} autoConfirm
+ * @property {RequestHooks} hooks
  * @property {boolean} analytics
  * @property {*} [store]
  * @property {*} [context]
@@ -31,6 +37,8 @@ export function buildOptions(options) {
   opts.version = opts.version || 1;
   opts.endpoint = (opts.endpoint || 'https://participants.evolv.ai/') + 'v' + opts.version;
   opts.analytics = 'analytics' in opts ? opts.analytics : opts.version > 1;
+
+  opts.hooks = assign({}, opts.hooks);
 
   return opts;
 }
