de284842d17007130837ed7c1d7480989855584f
Alireza Kasaaian <alirezakasaaian@gmail.com>
2022-08-25 13:37:06 +0000

    fix(ap-ap-1844): added limit to analytic beacon retries.

---
+++ src/tests/beacon.test.js
@@ -0,0 +1,50 @@
+import chai from 'chai';
+import spies from 'chai-spies';
+
+import Beacon, { RETRIES } from '../beacon.js';
+
+chai.use(spies);
+const expect = chai.expect;
+describe('beacon', () => {
+  const endpointV2 = 'https://participants-frazer.evolv.ai/v2';
+  beforeEach(() => {
+    global.windowRef = global.window
+  })
+  afterEach(() => {
+    global.window = global.windowRef
+  })
+
+  it('emit without infinite loop', (done) => {
+    console.log('test start')
+    global.window = { addEventListener: () => null , navigator: { sendBeacon: () => false } };
+    const beacon = new Beacon(endpointV2, { uid: '' }, '');
+    let spy = chai.spy.on(console, 'error')
+
+    beacon.emit('error', '{msg', true)
+    beacon.emit('error', '{msg', true)
+    beacon.emit('error', '{msg', true)
+    beacon.emit('error', '{msg', true)
+    beacon.emit('error', '{msg', true)
+    beacon.emit('error', '{msg', true)
+    beacon.emit('error', '{msg', true)
+    beacon.emit('error', '{msg', true)
+    beacon.emit('error', '{msg', true)
+
+    setTimeout(() => {
+      const firstState = 9 + RETRIES.max - 1
+      expect(spy).to.have.been.called(firstState);
+
+      beacon.emit('error', '{msg', true)
+      beacon.emit('error', '{msg', true)
+      beacon.emit('error', '{msg', true)
+      beacon.emit('error', '{msg', true)
+
+      setTimeout(() => {
+        const secondState = firstState + 4 + RETRIES.max - 1
+        expect(spy).to.have.been.called(secondState);
+        done()
+      }, 500)
+    }, 500)
+
+  });
+});
