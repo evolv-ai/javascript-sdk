6b21539f2865e3345349e0fb21eab7f53a48269b
Alireza Kasaaian <alirezakasaaian@gmail.com>
2022-08-31 19:02:53 +0100

    fix(ap-1844): added limit to analytic beacon retries. (#127)
    * fix(ap-ap-1844): added limit to analytic beacon retries.
    
    * fix(ap-ap-1844): refactored analytic beacon retries.
    
    * chore(AP-1844): housekeeping
    
    Co-authored-by: Matt Strom <5853934+mattstrom@users.noreply.github.com>

--- src/beacon.js
+++ src/beacon.js
@@ -4,6 +4,7 @@ import { assign, omitUndefined } from './ponyfills/objects.js';
 const DELAY = 1;
 const ENDPOINT_PATTERN = /\/(v\d+)\/\w+\/([a-z]+)$/i;
 const BATCH_SIZE = 25;
+export const RETRIES = 3;
 
 function fallbackBeacon(url, data, sync) {
   retrieve({
@@ -66,7 +67,10 @@ export default function Emitter(endpoint, context, options) {
     }
   }
 
-  function transmit() {
+  /**
+   * @param retries
+   */
+  function transmit(retries = RETRIES) {
     let sync = false;
     if (typeof this !== 'undefined' && this !== null) {
       const currentEvent = this.event && this.event.type;
@@ -90,8 +94,8 @@ export default function Emitter(endpoint, context, options) {
         let editedMessage = message;
         editedMessage = message.payload || {};
         editedMessage.type = message.type;
-
-        if (!send(endpoint, JSON.stringify(editedMessage), sync)) {
+        if (!send(endpoint, JSON.stringify(editedMessage), sync) && retries) {
+          retries -= 1;
           messages.push(message);
           console.error('Evolv: Unable to send event beacon');
         }
@@ -104,8 +108,9 @@ export default function Emitter(endpoint, context, options) {
           break;
         }
 
-        if (!send(endpoint, JSON.stringify(wrapMessages(smallBatch)), sync)) {
-          messages = batch
+        if (!send(endpoint, JSON.stringify(wrapMessages(smallBatch)), sync) && retries) {
+          retries -= 1;
+          messages = smallBatch;
           console.error('Evolv: Unable to send analytics beacon');
           break;
         }
@@ -115,7 +120,7 @@ export default function Emitter(endpoint, context, options) {
     }
 
     if (messages.length) {
-      timer = setTimeout(transmit, DELAY);
+      timer = setTimeout(function() {transmit(retries)}, DELAY);
     }
   }
 
