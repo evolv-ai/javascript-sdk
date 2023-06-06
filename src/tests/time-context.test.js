32f7967583efcaa02243f28229304b9b4ea14a34
joshua-barnard-evolv <78709720+joshua-barnard-evolv@users.noreply.github.com>
2023-06-06 03:34:44 +0000

    feat(AP-2109): Changed the path for each timer option

--- src/tests/time-context.test.js
+++ src/tests/time-context.test.js
@@ -32,28 +32,28 @@ describe('updateTimeContext', () => {
     describe('initial time set', () => {
         it('should set the correct utc time info on the local context', () => {
             addDateTimeToContext(context);
-            expect(context.get('utc.time.minute')).to.equal(12);
-            expect(context.get('utc.time.hour')).to.equal(11);
-            expect(context.get('utc.date.day')).to.equal(14);
-            expect(context.get('utc.date.month')).to.equal(6);
-            expect(context.get('utc.date.year')).to.equal(2017);
+            expect(context.get('dateTime.utc.time.minute')).to.equal(12);
+            expect(context.get('dateTime.utc.time.hour')).to.equal(11);
+            expect(context.get('dateTime.utc.date.day')).to.equal(14);
+            expect(context.get('dateTime.utc.date.month')).to.equal(6);
+            expect(context.get('dateTime.utc.date.year')).to.equal(2017);
         });
 
         it('should set the correct local time info on the local context', () => {
             addDateTimeToContext(context);
-            expect(context.get('time.minute')).to.equal(12);
+            expect(context.get('dateTime.local.time.minute')).to.equal(12);
             // Cannot override the timezone, so we can't test the local time
-            // expect(context.get('time.hour')).to.equal(4);
-            expect(context.get('date.day')).to.equal(14);
-            expect(context.get('date.month')).to.equal(6);
-            expect(context.get('date.year')).to.equal(2017);
+            // expect(context.get('dateTime.local.time.hour')).to.equal(4);
+            expect(context.get('dateTime.local.date.day')).to.equal(14);
+            expect(context.get('dateTime.local.date.month')).to.equal(6);
+            expect(context.get('dateTime.local.date.year')).to.equal(2017);
         });
 
         it('should set the correct day of week and hour of day on the local context', () => {
             addDateTimeToContext(context);
-            expect(context.get('dayOfWeek')).to.equal(3);
+            expect(context.get('dateTime.local.dayOfWeek')).to.equal(3);
             // Cannot override the timezone, so we can't test the local time
-            // expect(context.get('hourOfDay')).to.equal(4);
+            // expect(context.get('dateTime.local.hourOfDay')).to.equal(4);
         });
     });
 
@@ -65,56 +65,56 @@ describe('updateTimeContext', () => {
             clock = useFakeTimers(testDate.getTime());
 
             addDateTimeToContext(context);
-            expect(context.get('utc.time.minute')).to.equal(59);
-            expect(context.get('utc.time.hour')).to.equal(23);
-            expect(context.get('utc.date.day')).to.equal(31);
-            expect(context.get('utc.date.month')).to.equal(12);
-            expect(context.get('utc.date.year')).to.equal(2017);
+            expect(context.get('dateTime.utc.time.minute')).to.equal(59);
+            expect(context.get('dateTime.utc.time.hour')).to.equal(23);
+            expect(context.get('dateTime.utc.date.day')).to.equal(31);
+            expect(context.get('dateTime.utc.date.month')).to.equal(12);
+            expect(context.get('dateTime.utc.date.year')).to.equal(2017);
 
             // Cannot override the timezone, so we can't test the local time
-            /*expect(context.get('time.minute')).to.equal(59);
-            expect(context.get('time.hour')).to.equal(15);
-            expect(context.get('date.day')).to.equal(31);
-            expect(context.get('date.month')).to.equal(12);
-            expect(context.get('date.year')).to.equal(2017);*/
+            /*expect(context.get('dateTime.local.time.minute')).to.equal(59);
+            expect(context.get('dateTime.local.time.hour')).to.equal(15);
+            expect(context.get('dateTime.local.date.day')).to.equal(31);
+            expect(context.get('dateTime.local.date.month')).to.equal(12);
+            expect(context.get('dateTime.local.date.year')).to.equal(2017);*/
 
             clock.tick(120000);
 
-            expect(context.get('utc.time.minute')).to.equal(1);
-            expect(context.get('utc.time.hour')).to.equal(0);
-            expect(context.get('utc.date.day')).to.equal(1);
-            expect(context.get('utc.date.month')).to.equal(1);
-            expect(context.get('utc.date.year')).to.equal(2018);
+            expect(context.get('dateTime.utc.time.minute')).to.equal(1);
+            expect(context.get('dateTime.utc.time.hour')).to.equal(0);
+            expect(context.get('dateTime.utc.date.day')).to.equal(1);
+            expect(context.get('dateTime.utc.date.month')).to.equal(1);
+            expect(context.get('dateTime.utc.date.year')).to.equal(2018);
 
             // Cannot override the timezone, so we can't test the local time
-            /*expect(context.get('time.minute')).to.equal(1);
-            expect(context.get('time.hour')).to.equal(16);
-            expect(context.get('date.day')).to.equal(31);
-            expect(context.get('date.month')).to.equal(12);
-            expect(context.get('date.year')).to.equal(2017);*/
+            /*expect(context.get('dateTime.local.time.minute')).to.equal(1);
+            expect(context.get('dateTime.local.time.hour')).to.equal(16);
+            expect(context.get('dateTime.local.date.day')).to.equal(31);
+            expect(context.get('dateTime.local.date.month')).to.equal(12);
+            expect(context.get('dateTime.local.date.year')).to.equal(2017);*/
         });
     });
 
     describe('polling time updates', () => {
         it('should not update the time context if continueTimeUpdate is false', () => {
             addDateTimeToContext(context, false);
-            expect(context.get('time.minute')).to.equal(12);
+            expect(context.get('dateTime.local.time.minute')).to.equal(12);
             clock.tick(60000);
-            expect(context.get('time.minute')).to.equal(12);
+            expect(context.get('dateTime.local.time.minute')).to.equal(12);
         });
 
         it('should update the time context every minute if no value passed in - default is true', () => {
             addDateTimeToContext(context);
-            expect(context.get('time.minute')).to.equal(12);
+            expect(context.get('dateTime.local.time.minute')).to.equal(12);
             clock.tick(60000);
-            expect(context.get('time.minute')).to.equal(13);
+            expect(context.get('dateTime.local.time.minute')).to.equal(13);
         });
 
         it('should update the time context every minute', () => {
             addDateTimeToContext(context, true);
-            expect(context.get('time.minute')).to.equal(12);
+            expect(context.get('dateTime.local.time.minute')).to.equal(12);
             clock.tick(60000);
-            expect(context.get('time.minute')).to.equal(13);
+            expect(context.get('dateTime.local.time.minute')).to.equal(13);
         });
     });
 });
